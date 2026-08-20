cf9fe3c681815aba1957a5d3ad71941507d72796
RiyaRaghav <riyaraghavofficial@gmail.com>
2026-08-20 17:08:32 +0530

    Week 2 API assignment

---
+++ API_Assignment/convert_json_to_csv.py
@@ -0,0 +1,12 @@
+import json
+import pandas as pd
+
+with open("Bluestock_Week2/API_Assignment/posts_user1.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+df = pd.DataFrame(data)
+
+df.to_csv("posts_user1.csv", index=False)
+
+print("JSON successfully converted to CSV.")
+print(df)
